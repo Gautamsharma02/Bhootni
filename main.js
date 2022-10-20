e6e539ecad84bf5886c6df49b30da4029e542a39
Gautamsharma02 <gautamji94624@gmail.com>
2022-10-21 01:46:18 +0530

    second day

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 $("#clickBtn").click(function() {
     $('#left').toggle('left')
-})
+});
+
 $("#clickBtn").click(function() {
     $('#right').toggle('right')
-})
+});
+$("#clickBtn").click(function() {
+    $('#nextBtn').addClass('block')
+});
+
+
+$("#nextBtn").click(function() {
+    $('#span1, #clickBtn').addClass('none')
+});
+
+$("#nextBtn").click(function() {
+    $('#span2 ').addClass('block')
+});
